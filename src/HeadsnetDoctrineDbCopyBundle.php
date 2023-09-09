948cde6880bb8bf8806baacb73a4ca5ec9f571bc
Ben Roberts <ben@headsnet.com>
2023-09-09 09:21:56 +0200

    Add bundle file

---
+++ src/HeadsnetDoctrineDbCopyBundle.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace Headsnet\DoctrineDbCopyBundle;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class HeadsnetDoctrineDbCopyBundle extends AbstractBundle
+{
+
+}
