1104ce67d790c1a9c2eb08c22526d27cd31217cb
Ben Roberts <ben@headsnet.com>
2023-09-09 09:15:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+Doctrine Copy DB Bundle
+====
+
+This bundle provides a Symfony command to copy an entire database schema, without any dependency on 
+database client binaries such as `mysqldump` or `mysql`.
+
+The primary use case is for duplicating a test database for use with [Paratest](https://github.com/paratestphp/paratest) 
+in CI, where access to the database binaries is not possible, for example due to them being inside a Docker 
+container that only exposes the MySQL port 3306.
+
+This solution is significantly faster than dumping an SQL file and then importing it.
+
+### Usage
+
+To copy `test_1` to `test_2`, simply run:
+
+```bash
+bin/console headsnet:copy-db -s test_1 -d test_2;
+```
+
+To create multiple test database copies, you can use a for loop:
+
+```bash
+PARATEST_THREADS=6
+
+for (( i = 2; i <= $PARATEST_THREADS; i++ )); do
+    bin/console headsnet:copy-db -s test_1 -d test_$i;
+done
+```
+
+### Licence
+
+This code is released under the MIT licence. Please see the LICENSE file for more information.
---
+++ src/Console/CopyDbCommand.php
@@ -0,0 +1,90 @@
+<?php
+declare(strict_types=1);
+
+namespace Headsnet\DoctrineDbCopyBundle\Console;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Result;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: 'headsnet:copy-db',
+    description: 'Copies a database using only SQL commands'
+)]
+final class CopyDbCommand extends Command
+{
+    public function __construct(
+        private readonly Connection $connection
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addOption(
+                'source',
+                's',
+                InputOption::VALUE_REQUIRED,
+                'The name of the source database'
+            )
+            ->addOption(
+                'destination',
+                'd',
+                InputOption::VALUE_REQUIRED,
+                'The name of the destination database'
+            )
+        ;
+    }
+
+    /**
+     * @throws Exception
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $sourceDb = $input->getOption('source');
+        $destinationDb = $input->getOption('destination');
+
+        // Create target database
+        $this->query(sprintf('DROP DATABASE IF EXISTS %s', $destinationDb));
+        $this->query(sprintf('CREATE DATABASE %s', $destinationDb));
+
+        // Disable foreign key checks
+        $this->query('SET FOREIGN_KEY_CHECKS = 0');
+
+        // Load all tables names from source database
+        $result = $this->query('SHOW TABLES');
+        $tables = $result->fetchAllAssociative();
+        $tables = array_map(fn (array $table): string => $table['Tables_in_' . $sourceDb], $tables);
+
+        // Loop over tables and...
+        foreach ($tables as $table)
+        {
+            // create tables in the new database
+            $this->query(sprintf('CREATE TABLE %s.%s LIKE %s.%s', $destinationDb, $table, $sourceDb, $table));
+
+            // copy table data to new database
+            $this->query(sprintf('INSERT INTO %s.%s SELECT * FROM %s.%s', $destinationDb, $table, $sourceDb, $table));
+        }
+
+        // Re-enable foreign key checks
+        $this->query('SET FOREIGN_KEY_CHECKS = 1');
+
+        return Command::SUCCESS;
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function query(string $sql): Result
+    {
+        $stmt = $this->connection->prepare($sql);
+
+        return $stmt->executeQuery();
+    }
+}
